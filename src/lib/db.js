e3e6effaa7842abba84ab3c7f2145be96d96904a
ThibaultTheProG <tuffinthibaultgw@gmail.com>
2025-02-15 13:29:59 +0100

    Modification du système de connexion à la BDD

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,14 +2,15 @@ import pg from "pg";
 
 const { Pool } = pg;
 const db = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString:
+    process.env.NODE_ENV === "production"
+      ? process.env.DB_URL
+      : process.env.DB_URL_PREPROD, // Pooling activé
 });
 
-db.on("connect", () => console.log("Connecté au pool de base de données"));
-db.on("error", (err) => console.error("Erreur avec le pool de base de données :", err));
+db.on("connect", () => console.log("Connecté au pool de base de données : ", process.env.NODE_ENV));
+db.on("error", (err) =>
+  console.error("Erreur avec le pool de base de données :", err)
+);
 
 export default db;
