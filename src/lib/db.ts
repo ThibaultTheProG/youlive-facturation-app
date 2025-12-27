84b7e143e752b804b7e6754cc6700915c1ab2f19
ThibaultTheProG <tuffinthibaultgw@gmail.com>
2025-12-27 10:17:04 +0100

    prisma config

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,19 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
 const prisma = global.prisma || new PrismaClient({
+  adapter,
   log: ['query', 'error', 'warn'],
 });
 
